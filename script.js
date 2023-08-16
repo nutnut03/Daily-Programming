d2c8a80b9ca667e3cef0c7a9e30f840c8a183237
nutnut <carlo.meriseinc@gmail.com>
2023-08-16 16:23:42 +0800

    1% all updates

---
+++ script.js
@@ -0,0 +1,9 @@
+// script for navagition bar responsive
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
